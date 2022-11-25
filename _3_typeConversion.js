2e2b52723ceadf36dcb29fce929c9e2b36ee9fa9
KonstantRock <eradicated47@yandex.ru>
2022-11-25 23:58:46 +0300

    first commit

---
+++ _3_typeConversion.js
@@ -0,0 +1,17 @@
+let a = null == 0 // a = false
+
+let b = null >= 0 // b = true
+
+let c = null == undefined // c = true
+
+let d = 4 + 5 + "px" // d = "9px"
+
+let e = "  9  " + 5 // e = "  9  5"
+
+let f = "  9  " - 5 // f = 4
+
+let g = null + 1 // g = 1
+
+let h = undefined + 1 // h = NaN
+
+let i = " \t \h " - 2 // i = -2
